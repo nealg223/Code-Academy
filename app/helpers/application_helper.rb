9599af8b4b2d0a8cd563f6dc2fd8fdc37349f616
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-06-27 20:35:49 -0500

    got rid of the extra title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,6 @@ module ApplicationHelper
   # Return a title on a per-page basis
   def title
     base_title = "Code Academy"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
   end
   
   def tweet
