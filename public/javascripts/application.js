8a453727e1b696ab9dc9a70a4ffee80d55dcd931
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-07-01 22:20:53 -0500

    edited the content

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 	        var targetOffset = $target.offset().top;
 	        $(this).click(function(event) {
 	          event.preventDefault();
-	          $(scrollElem).animate({scrollTop: targetOffset}, 1000, function() {
+	          $(scrollElem).animate({scrollTop: targetOffset}, 500, function() {
 	            location.hash = target;
 	          });
 	        });
