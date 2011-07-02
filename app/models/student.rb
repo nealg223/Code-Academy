33fe5b1dcccdea8e7961644771218a0345d19c1b
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-07-02 14:33:57 -0500

    updated some of the content, edited some css

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,95 @@
 class Student < ActiveRecord::Base
-  
+  # def mantra
+  #   base_mantra = "Become a"
+  #   if @mantra.nil?
+  #     base_title
+  #   else
+  #     "#{base_mantra} #{@mantra}"
+  #   end
+  # end
 end
+
+# class You < CodeAcademy::Chicago
+#   
+#   def builders_wanted  
+#     ca = code_academy
+#     @passion          = Self.new
+#     @technology       = Self.
+#     @entrepreneurship = 
+#   end
+#   
+#   def skills
+#     
+#     def entrepreneurship
+#       @startups
+#     end
+# 
+#     def technology
+#       
+#       def ruby_on_rails
+#         
+#       end
+# 
+#     end
+#   end
+#   
+#   def support
+#     
+#     def instruction
+#       def curriculum
+# 
+#       end
+#     end
+# 
+#     def mentorship
+# 
+#     end
+#     
+#     def ca_team
+#       
+#     end
+#   end
+#   
+#   def commitment
+#     
+#     def hours
+#       
+#     end
+#     
+#     def location
+#       
+#     end
+#     
+#     def investment
+#       
+#     end
+#     
+#   end
+#   
+#   def testimonials
+#     
+#     def peers
+#       
+#     end
+#     
+#     def investors
+#       
+#     end
+#     
+#     def entrepeneurs
+#       
+#     end
+#     
+#     def developers
+#       
+#     end
+#     
+#   end
+# 
+#   def happiness
+#     if @purpose.exists?
+#       return "#{ca} = #{@passion} + #{@code}"
+#     else
+#       You.nil
+#   end
+# end
